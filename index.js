4c738bec32f56a19d6798a9d8e4d2ffe9c0f9d3e
James Saint <JamesSaint@users.noreply.github.com>
2025-11-09 21:54:08 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // Update this list when you add a page.
 // Order doesn't matter; code sorts by any date in the filename.
 const PAGES = [
- // "When-Love-Stops-Being-Shared_2025-11-10.html",
+  "When-Love-Stops-Being-Shared_2025-11-10.html",
  "2025-11-07-paddy-feels-it-too.html",
   "Why-I-Stayed-2025-11-06.html",
   "Naming_What_It_Was_2025-11-05.html",
