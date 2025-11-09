71ac840b3c2434431b8f9348998912a03b91fb68
James Saint <JamesSaint@users.noreply.github.com>
2025-11-09 19:40:28 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -3,22 +3,24 @@
 // Update this list when you add a page.
 // Order doesn't matter; code sorts by any date in the filename.
 const PAGES = [
+ // "When-Love-Stops-Being-Shared_2025-11-11.html",
   "Why-I-Stayed-2025-11-06.html",
   "Naming_What_It_Was_2025-11-05.html",
   "Little_Wonder_I_Feel_So_Unwell_2025-11-04.html",
   "Authorship-2025-11-05.html",
-//  "Transparency-And-Ai-Assistive-Tools-2025-11-04.html",
+// "Transparency-And-Ai-Assistive-Tools-2025-11-04.html",
   "Why_People_Fear_Those_Who_See_Too_Much_2025-11-03.html",
   "How_to_Approach_Pain_2025-10-30.html",
  // "Louisa_Witness_2025-11-02.html",
   "The_Turning_Point_2025-11-02.html",
   "The_Loop_Closes_2025-11-01.html",
   "The_First_Dose_2025-10-31.html",
+  "Witnessing-What-is-Real_2025-10-29.html",
   "what_I_have_carried_29-10-2025.html",
   "A_Letter_to_the_Ghost_of_a_Friend_2025-10-28.html",
   "Witness_of_1996_2025-10-28.html",
   "while-she-finds-her-way-home-2025-10-27.html",
-  "Finding_My_Kind_26-10-2025.html",
+// "Finding_My_Kind_26-10-2025.html",
   "Declaration_Stand_in_Truth_2025-10-26.html",
   "Letter_to_Mum_2025-10-26.html",
   "Speaking_into_the_machinery_21-10-2025.html",
@@ -26,7 +28,7 @@ const PAGES = [
  // "Choice_Disguised_as_Necessity_2025-10-15.html",
 //  "Truth_is_Love_2025-10-13.html",
   "Reparenting_Transmission_09-10-2025.html",
-  "The_Overlay_2025-07-07.html",
+  //"The_Overlay_2025-07-07.html",
   "Grounding_The_Storm_Circuit.html"
 ];
 
